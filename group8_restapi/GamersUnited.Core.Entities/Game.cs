24f0149b4f6e236cb810676c9d49671d2a5ee749
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 12:08:52 +0200

    Added game entity

---
+++ group8_restapi/GamersUnited.Core.Entities/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamersUnited.Core.Entities
+{
+    class Game
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public GameGenre Genre { get; set; }
+    }
+}

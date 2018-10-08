b5ae214d47bb62a51bb94f011816085110b47ae2
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 15:51:36 +0200

    Unit test adding

---
+++ group8_restapi/GamersUnited.Infrastructure.Data/ProductCategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using GamersUnited.Core.DomainService;
+using GamersUnited.Core.Entities;
+using GamersUnited.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamersUnited.Infrastructure.Data
+{
+    public class ProductCategoryRepository : IRepository<ProductCategory>
+    {
+        readonly GamersUnitedContext _ctx;
+
+        public ProductCategoryRepository(GamersUnitedContext context)
+        {
+            _ctx = context;
+        }
+
+        public ProductCategory Add(ProductCategory obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductCategory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory Remove(ProductCategory obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory Update(int id, ProductCategory obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ group8_restapi/XUnitTestCore/Infrastructure/Data/ProductCategoryTest.cs
@@ -0,0 +1,43 @@
+﻿using GamersUnited.Core.Entities;
+using GamersUnited.Infrastructure.Data;
+using GamersUnited.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace XUnitTestCore.Infrastructure.Data
+{
+    public class ProductCategoryTest
+    {
+        [Theory]
+        [InlineData(1, "Testing category")]
+        [InlineData(2, "category")]
+        [InlineData(3, "Testing")]
+        public void CreateValidProductCategoryTest(int id, string name)
+        {
+            var options = new DbContextOptionsBuilder<GamersUnitedContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            var pc = new ProductCategory() { Id = id, Name = name };
+
+            // Run the test against one instance of the context
+            using (var context = new GamersUnitedContext(options))
+            {
+                var repo = new ProductCategoryRepository(context);
+                repo.Add(pc);
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new GamersUnitedContext(options))
+            {
+                Assert.Equal(1, context.ProductCategory.Count());
+                Assert.Equal(id, context.ProductCategory.Single().Id);
+                Assert.Equal(name, context.ProductCategory.Single().Name);
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}

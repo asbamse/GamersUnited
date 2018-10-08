644396bdb7d0f3d67b5c8b7325b2a0799f17f5ed
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 10:12:06 +0200

    Added repository interface

---
+++ group8_webpage/Core/DomainService/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DomainService
+{
+    public interface IRepository<T>
+    {
+        int Count();
+        T Add(T obj);
+        T GetById(int id);
+        IList<T> GetAll();
+        T Update(int id, T obj);
+        T Remove(T obj);
+    }
+}

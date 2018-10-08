84738cae700e452f7ca296af19577962b659eb8d
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 14:55:01 +0200

    Added database context

---
+++ group8_restapi/GamersUnited.Infrastructure.Data.Context/GamersUnitedContext.cs
@@ -0,0 +1,64 @@
+﻿using GamersUnited.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GamersUnited.Infrastructure.Data.Context
+{
+    public class GamersUnitedContext : DbContext
+    {
+        public GamersUnitedContext(DbContextOptions<GamersUnitedContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne<ProductCategory>(p => p.Category)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Game>()
+                .HasOne<Product>(g => g.Product)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Game>()
+                .HasOne<GameGenre>(g => g.Genre)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Stock>()
+                .HasOne<Product>(s => s.Product)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Sold>()
+                .HasOne<Product>(s => s.Product)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<SoldInvoiceRelation>().HasKey(si => new { si.SoldId, si.InvoiceId });
+
+            modelBuilder.Entity<SoldInvoiceRelation>()
+                .HasOne<Sold>(s => s.Sold)
+                .WithMany()
+                .HasForeignKey(si => si.SoldId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<SoldInvoiceRelation>()
+                .HasOne<Invoice>(i => i.Invoice)
+                .WithMany(s => s.Products)
+                .HasForeignKey(si => si.InvoiceId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<GameGenre> GameGenre { get; set; }
+        public DbSet<Game> Game { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Sold> Sold { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+    }
+}

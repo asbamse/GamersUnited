d7cd3a9103f19f493e8a49862dd1a3e8a474f2bb
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 12:16:54 +0200

    Added user and invoice entity

---
+++ group8_restapi/GamersUnited.Core.Entities/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamersUnited.Core.Entities
+{
+    class Invoice
+    {
+        public int Id { get; set; }
+        public Sold Product { get; set; }
+        public User Customer { get; set; }
+        public DateTime DateSold { get; set; }
+    }
+}

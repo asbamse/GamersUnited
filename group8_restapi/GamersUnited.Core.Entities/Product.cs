0aeb3cdb8f8a46a420454bb6da328f2e6a290469
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-08 12:06:38 +0200

    Added product entity

---
+++ group8_restapi/GamersUnited.Core.Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamersUnited.Core.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProductCategory Category { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}

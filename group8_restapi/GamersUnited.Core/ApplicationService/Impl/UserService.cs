9bab92af74419ea7289a5decf4edf790c76efacd
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-09 17:28:09 +0200

    UserService: Data validation

--- group8_restapi/GamersUnited.Core/ApplicationService/Impl/UserService.cs
+++ group8_restapi/GamersUnited.Core/ApplicationService/Impl/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GamersUnited.Core.ApplicationService.Impl.Utilities;
 using GamersUnited.Core.DomainService;
 using GamersUnited.Core.Entities;
 
@@ -17,6 +18,7 @@ namespace GamersUnited.Core.ApplicationService.Impl
 
         public User Add(User user)
         {
+            ValidateData(user);
             return _ur.Add(user);
         }
 
@@ -37,7 +39,27 @@ namespace GamersUnited.Core.ApplicationService.Impl
 
         public User Update(int id, User user)
         {
+            ValidateData(user);
             return _ur.Update(id, user);
+        }
+
+        private void ValidateData(User user)
+        {
+            Requirement.MinLength(2, user.FirstName, "FirstName");
+            Requirement.MaxLength(50, user.FirstName, "FirstName");
+
+            Requirement.MinLength(2, user.LastName, "LastName");
+            Requirement.MaxLength(50, user.LastName, "LastName");
+
+            Requirement.MinLength(2, user.Address, "Address");
+            Requirement.MaxLength(50, user.Address, "Address");
+
+            Requirement.Email(user.Email, "Email");
+
+            Requirement.DanishPhoneNumber(user.PhoneNumber, "PhoneNumber");
+
+            Requirement.MinLength(8, user.Password, "Password");
+            user.Password = Cryptography.Encrypt(user.Password);
         }
     }
 }

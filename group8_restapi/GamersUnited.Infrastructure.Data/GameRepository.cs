a6e34bab322460af5700dc60302d0a6dcf031d87
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-10-09 13:14:48 +0200

    GameRepository: Copied ProductRepository

---
+++ group8_restapi/GamersUnited.Infrastructure.Data/GameRepository.cs
@@ -0,0 +1,164 @@
+﻿using GamersUnited.Core.DomainService;
+using GamersUnited.Core.Entities;
+using GamersUnited.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GamersUnited.Infrastructure.Data
+{
+    public class GameRepository : IRepository<Game>
+    {
+        readonly GamersUnitedContext _ctx;
+        readonly IRepository<Product> _pr;
+        readonly IRepository<GameGenre> _ggr;
+
+        public GameRepository(GamersUnitedContext context, IRepository<Product> productRepository, IRepository<GameGenre> gameGenreRepository)
+        {
+            _ctx = context;
+            _pr = productRepository;
+            _ggr = gameGenreRepository;
+        }
+
+        public Game Add(Game obj)
+        {
+            if (obj.Product == null)
+            {
+                throw new ArgumentNullException("The product cannot be null");
+            }
+            else if (obj.Genre == null)
+            {
+                throw new ArgumentNullException("The game genre cannot be null");
+            }
+
+            Product np;
+            if(obj.Product.Id > 0)
+            {
+                try
+                {
+                    np = _pr.GetById(obj.Product.Id);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    np = _pr.Add(obj.Product);
+                }
+            }
+            else
+            {
+                np = _pr.Add(obj.Product);
+            }
+
+            GameGenre ngg;
+            if (obj.Genre.Id > 0)
+            {
+                try
+                {
+                    ngg = _ggr.GetById(obj.Genre.Id);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    ngg = _ggr.Add(obj.Genre);
+                }
+            }
+            else
+            {
+                ngg = _ggr.Add(obj.Genre);
+            }
+
+            var tmp = new Game { Product = np, Genre = ngg };
+
+            Game item = _ctx.Game.Add(tmp).Entity;
+            _ctx.SaveChanges();
+
+            return item;
+        }
+
+        public int Count()
+        {
+            return _ctx.Game.Count();
+        }
+
+        public IList<Game> GetAll()
+        {
+            return _ctx.Game.ToList();
+        }
+
+        public Game GetById(int id)
+        {
+            var item = _ctx.Game.FirstOrDefault(b => b.Id == id);
+
+            if (item == null)
+            {
+                throw new ArgumentOutOfRangeException("Id not found!");
+            }
+
+            return item;
+        }
+
+        public Game Remove(Game obj)
+        {
+            var item = GetById(obj.Id);
+
+            _ctx.Game.Remove(item);
+            _ctx.SaveChanges();
+
+            return item;
+        }
+
+        public Game Update(int id, Game obj)
+        {
+            if (obj.Product == null)
+            {
+                throw new ArgumentNullException("The product cannot be null");
+            }
+            else if (obj.Genre == null)
+            {
+                throw new ArgumentNullException("The game genre cannot be null");
+            }
+
+            Product np;
+            if (obj.Product.Id > 0)
+            {
+                try
+                {
+                    np = _pr.GetById(obj.Product.Id);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    np = _pr.Add(obj.Product);
+                }
+            }
+            else
+            {
+                np = _pr.Add(obj.Product);
+            }
+
+            GameGenre ngg;
+            if (obj.Genre.Id > 0)
+            {
+                try
+                {
+                    ngg = _ggr.GetById(obj.Genre.Id);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    ngg = _ggr.Add(obj.Genre);
+                }
+            }
+            else
+            {
+                ngg = _ggr.Add(obj.Genre);
+            }
+
+            var item = GetById(id);
+            item.Product = np;
+            item.Genre = ngg;
+            
+            item = _ctx.Game.Update(item).Entity;
+            _ctx.SaveChanges();
+
+            return item;
+        }
+    }
+}
